2267ca8b20c523c20288c35a42b8b76685e781c6
Aditi Singh <64026259+aditisingh0409@users.noreply.github.com>
2023-10-18 16:59:46 +0530

    Update 34a-client.c

--- 34a-client.c
+++ 34a-client.c
@@ -17,59 +17,45 @@ Date: 10th Oct, 2023.
 
 void main()
 {
-    int socketFD, connectionFD, bindStatus, listenStatus, clientSize;
-    struct sockaddr_in address, client;
-    ssize_t readBytes, writeBytes;
+    int socketFD, connectStatus; 
+    struct sockaddr_in address;
+    ssize_t readBytes, writeBytes; 
     char buffer[100];
 
+    // Create a socket for TCP connection using IPv4
     socketFD = socket(AF_INET, SOCK_STREAM, 0);
     if (socketFD == -1)
     {
         perror("Error while creating socket!");
         _exit(0);
     }
-    printf("Server side socket successfully created!\n");
+    printf("Client side socket successfully created!\n");
 
+    // Defined server's details
     address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_family = AF_INET;
     address.sin_port = htons(8080);
 
-    bindStatus = bind(socketFD, (struct sockaddr *)&address, sizeof(address));
-    if (bindStatus == -1)
+    connectStatus = connect(socketFD, (struct sockaddr *)&address, sizeof(address));
+    if (connectStatus == -1)
     {
-        perror("Error while binding name to socket!");
+        perror("Error while connecting to server!");
         _exit(0);
     }
-    printf("Binding to socket was successful!\n");
+    printf("Client to server connection successfully established!\n");
 
-    listenStatus = listen(socketFD, 2);
-    if (listenStatus == -1)
-    {
-        perror("Error while trying to listen for connections!");
-        _exit(0);
-    }
-    printf("Now listening for connections on a socket!\n");
-
-    clientSize = (int)sizeof(client);
-    connectionFD = accept(socketFD, (struct sockaddr *)&client, &clientSize);
-    if (connectionFD == -1)
-        perror("Error while accepting a connection!");
+    // Client - Server Communication
+    readBytes = read(socketFD, buffer, 100);
+    if (readBytes == -1)
+        perror("Error while reading from network via socket!");
     else
-    {
+        printf("Data from server: %s\n", buffer);
 
-        // Server - Client Communication
-        writeBytes = write(connectionFD, "I'm the server!", 15);
-        if (writeBytes == -1)
-            perror("Error while writing to network via socket!");
-        else
-            printf("Data sent to client!\n");
-
-        readBytes = read(connectionFD, buffer, 100);
-        if (readBytes == -1)
-            perror("Error while reading from network via socket!");
-        else
-            printf("Data from client: %s\n", buffer);
-    }
-    close(connectionFD);
+    writeBytes = write(socketFD, "I'm the client!", 15);
+    if (writeBytes == -1)
+        perror("Error while writing to network via socket!");
+    else
+        printf("Data sent to server!");
+    getchar();
     close(socketFD);
 }
